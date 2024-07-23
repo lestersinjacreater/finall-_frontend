e2b1250751c8a15f0eb1c98b6f919b877640fe84
lestersinjacreater <lestetsinjawatosia@gmail.com>
2024-07-23 22:33:53 +0300

    les style

--- src/pages/timelinepage/vehicles.tsx
+++ src/pages/timelinepage/vehicles.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const VehiclesList = () => {
     const { data: vehiclesData, error, isLoading } = VehicleAPI.useGetVehiclesQuery();
 
-    if (isLoading) return <div className="text-center mt-4">Loading...</div>;
-    if (error) return <div className="text-center mt-4 text-red-600">Error loading vehicles</div>;
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error loading vehicles</div>;
 
     // Function to format rental rate as currency
     function formatCurrency(rentalRate: number): React.ReactNode {
@@ -16,14 +16,14 @@ const VehiclesList = () => {
     }
 
     return (
-        <div className="container mx-auto p-4">
+        <div>
             {vehiclesData?.map(({ vehicles, vehicle_specs }) => (
-                <div key={vehicles.vehicleId} className="bg-white p-6 rounded-lg shadow-md mb-6">
+                <div key={vehicles.vehicleId} className="p-4">
                     <div className="flex justify-between items-center mb-4">
-                        <h3 className="text-2xl font-semibold text-teal-600">{`${vehicle_specs.manufacturer} ${vehicle_specs.model}`}</h3>
+                        <h3 className="text-xl font-semibold text-teal-600">{`${vehicle_specs.manufacturer} ${vehicle_specs.model}`}</h3>
                         <Link
                             to={`/bookings?vehicleId=${vehicles.vehicleId}&rentalRate=${vehicles.rentalRate}&vehicleSpecId=${vehicles.vehicleSpecId}`}
-                            className="bg-teal-500 text-white px-4 py-2 rounded-md font-bold hover:bg-teal-600 transition duration-300"
+                            className="bg-teal-500 text-white px-4 py-2 rounded-md inline-block font-bold hover:bg-teal-600"
                         >
                             Book Now
                         </Link>
