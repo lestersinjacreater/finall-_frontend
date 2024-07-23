f3b897599c10a3057c89972950ab132d31a1e93c
lestersinjacreater <lestetsinjawatosia@gmail.com>
2024-07-23 20:10:54 +0300

    vehicle listing

--- src/features/vehicles/vehiclesandspecifications.api.ts
+++ src/features/vehicles/vehiclesandspecifications.api.ts
@@ -1,37 +1,93 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export interface Vehicle {
-    vehicle_specs: any;
-    vehicleId: number;
-    vehicleSpecId: number;
-    rentalRate: string;
-    availability: boolean;
-    address: string;
-}
-export interface VehicleSpec {
-    vehicleSpecId:number;
-    nmanufactureYear: number;
-    model: string;
-    year: number;
-    engineCapacity: number;
-    transmission: string;
-    seatingCapacity: number;
-    color: string;
-    features: string;
-    fuelType: string;
-}
+// export interface Vehicle {
+//     vehicle_specs: any;
+//     vehicleId: number;
+//     vehicleSpecId: number;
+//     rentalRate: string;
+//     availability: boolean;
+//     address: string;
+// }
+// export interface VehicleSpec {
+//     vehicleSpecId:number;
+//     nmanufactureYear: number;
+//     model: string;
+//     year: number;
+//     engineCapacity: number;
+//     transmission: string;
+//     seatingCapacity: number;
+//     color: string;
+//     features: string;
+//     fuelType: string;
+//     image_url: string;
+// }
+export interface VehicleResponse {
+    vehicles: {
+      vehicleId: number;
+      vehicleSpecId: number;
+      rentalRate: number;
+      availability: boolean;
+      createdAt: string;
+      updatedAt: string;
+      address: string;
+    };
+    vehicle_specs: {
+      vehicleSpecId: number;
+      manufacturer: string;
+      model: string;
+      year: number;
+      fuelType: string;
+      engineCapacity: number;
+      transmission: string;
+      seatingCapacity: number;
+      color: string;
+      features: string;
+      image_url: string;
+    };
+  }
+
+
 
 
 export const VehicleAPI = createApi({
     reducerPath: 'VehicleAPI',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://drill-wheel-rental-system-backend.onrender.com/api' }),
     endpoints: (builder) => ({
-        getVehicles: builder.query<Vehicle[], void>({
+        getVehicles: builder.query<VehicleResponse[], void>({
             query: () => 'vehiclesspecs', // Make sure this matches your API endpoint
             //providesTags: ['Vehicle']
-        }),
+            transformResponse: (Response: VehicleResponse[]) => {
+                return Response.map((item) => ({
+                    vehicles: {
+                        vehicleId: item.vehicles.vehicleId,
+                        vehicleSpecId: item.vehicles.vehicleSpecId,
+                        rentalRate: item.vehicles.rentalRate,
+                        availability: item.vehicles.availability,
+                        createdAt: item.vehicles.createdAt,
+                        updatedAt: item.vehicles.updatedAt,
+                        address: item.vehicles.address,
+                    },
+                    vehicle_specs: {
+                        vehicleSpecId: item.vehicle_specs.vehicleSpecId,
+                        manufacturer: item.vehicle_specs.manufacturer,
+                        model: item.vehicle_specs.model,
+                        year: item.vehicle_specs.year,
+                        fuelType: item.vehicle_specs.fuelType,
+                        engineCapacity: item.vehicle_specs.engineCapacity,
+                        transmission: item.vehicle_specs.transmission,
+                        seatingCapacity: item.vehicle_specs.seatingCapacity,
+                        color: item.vehicle_specs.color,
+                        features: item.vehicle_specs.features,
+                        image_url: item.vehicle_specs.image_url,
+                    },
+                    
+                }));
+            },
         
-        createVehicles: builder.mutation<Vehicle, Partial<Vehicle>>({
+
+}),
+        
+        createVehicles: builder.mutation<VehicleResponse, Partial<VehicleResponse>>({
             query: (newVehicle) => ({
                 url: 'Vehicles',
                 method: 'POST',
@@ -39,7 +95,7 @@ export const VehicleAPI = createApi({
             }),
            // invalidatesTags: ['Vehicle']
         }),
-        updateVehicles: builder.mutation<Vehicle, Partial<Vehicle> & { id: number }>({
+        updateVehicles: builder.mutation<VehicleResponse, Partial<VehicleResponse> & { id: number }>({
             query: ({ id, ...updatedVehicles }) => ({
                 url: `Vehicles/${id}`,
                 method: 'PUT',
@@ -56,6 +112,9 @@ export const VehicleAPI = createApi({
         }),
     }),
 });
+
+
+
 
 // export const {
 //     useGetVehiclesQuery,
--- src/pages/timelinepage/vehicles.tsx
+++ src/pages/timelinepage/vehicles.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { VehicleAPI, Vehicle } from '../../features/vehicles/vehiclesandspecifications.api';
+import { VehicleAPI, VehicleResponse } from '../../features/vehicles/vehiclesandspecifications.api';
 
 const Vehicles: React.FC = () => {
     const { data: vehicles, isLoading, isError } = VehicleAPI.useGetVehiclesQuery();
-    
+
     console.log('Vehicles Data:', JSON.stringify(vehicles, null, 2));
 
-    const formatCurrency = (value: string | undefined) => {
-        if (!value) {
+    const formatCurrency = (value: number | undefined) => {
+        if (value === undefined) {
             return 'N/A';
         }
-        const numberValue = parseFloat(value.replace(/[^0-9.-]+/g, ''));
-        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(numberValue);
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
     };
 
-    const handleBookNow = (vehicleId: number, rentalRate: string, bookingId: number) => {
+    const handleBookNow = (vehicleId: number, rentalRate: number, vehicleSpecId: number) => {
         localStorage.setItem("vehicleId", vehicleId.toString());
-        localStorage.setItem("rentalRate", rentalRate);
-        localStorage.setItem("bookingId", bookingId.toString());
+        localStorage.setItem("rentalRate", rentalRate.toString());
+        localStorage.setItem("vehicleSpecId", vehicleSpecId.toString());
     };
 
     if (isLoading) {
@@ -32,44 +31,45 @@ const Vehicles: React.FC = () => {
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {vehicles && vehicles.map((vehicle: Vehicle, index: number) => (
-                    <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                        {vehicle.vehicle_specs && (
-                            <>
-                                <img src={vehicle.vehicle_specs.image_url} alt={`${vehicle.vehicle_specs.manufacturer} ${vehicle.vehicle_specs.model}`} className="w-full h-48 object-cover mb-4" />
-                                <div className="p-4">
-                                    <div className="flex justify-between items-center mb-4">
-                                        <h3 className="text-xl font-semibold text-teal-600">{vehicle.vehicle_specs.manufacturer} {vehicle.vehicle_specs.model}</h3>
-                                        <Link
-                                            to="/bookings"
-                                            className="bg-teal-500 text-white px-4 py-2 rounded-md inline-block font-bold hover:bg-teal-600"
-                                            onClick={() => handleBookNow(vehicle.vehicleId, vehicle.rentalRate, vehicle.vehicleSpecId)}
-                                        >
-                                            Book Now
-                                        </Link>
-                                    </div>
-                                    <div className="mb-4">
-                                        <p className="text-lg font-semibold text-green-600">
-                                            <span className="text-gray-800">Rental Rate:</span> {formatCurrency(vehicle.rentalRate)}
-                                        </p>
-                                        <p className={`text-lg font-semibold ${vehicle.availability ? 'text-green-600' : 'text-red-600'}`}>
-                                            <span className="text-gray-800">Availability:</span> {vehicle.availability ? 'Available' : 'Unavailable'}
-                                        </p>
-                                    </div>
-                                    <div className="space-y-2 text-gray-700">
-                                        <p><strong>Year:</strong> {vehicle.vehicle_specs.year}</p>
-                                        <p><strong>Fuel Type:</strong> {vehicle.vehicle_specs.fuelType}</p>
-                                        <p><strong>Engine Capacity:</strong> {vehicle.vehicle_specs.engineCapacity}</p>
-                                        <p><strong>Transmission:</strong> {vehicle.vehicle_specs.transmission}</p>
-                                        <p><strong>Seating Capacity:</strong> {vehicle.vehicle_specs.seatingCapacity}</p>
-                                        <p><strong>Color:</strong> {vehicle.vehicle_specs.color}</p>
-                                        <p><strong>Features:</strong> {vehicle.vehicle_specs.features}</p>
-                                    </div>
+                {vehicles && vehicles.map((vehicleResponse: VehicleResponse, index: number) => {
+                    const vehicle = vehicleResponse.vehicles;
+                    const specs = vehicleResponse.vehicle_specs;
+
+                    return (
+                        <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                            <img src={specs.image_url} alt={`${specs.manufacturer} ${specs.model}`} className="w-full h-48 object-cover mb-4" />
+                            <div className="p-4">
+                                <div className="flex justify-between items-center mb-4">
+                                    <h3 className="text-xl font-semibold text-teal-600">{`${specs.manufacturer} ${specs.model}`}</h3>
+                                    <Link
+                                        to="/bookings"
+                                        className="bg-teal-500 text-white px-4 py-2 rounded-md inline-block font-bold hover:bg-teal-600"
+                                        onClick={() => handleBookNow(vehicle.vehicleId, vehicle.rentalRate, vehicle.vehicleSpecId)}
+                                    >
+                                        Book Now
+                                    </Link>
                                 </div>
-                            </>
-                        )}
-                    </div>
-                ))}
+                                <div className="mb-4">
+                                    <p className="text-lg font-semibold text-green-600">
+                                        <span className="text-gray-800">Rental Rate:</span> {formatCurrency(vehicle.rentalRate)}
+                                    </p>
+                                    <p className={`text-lg font-semibold ${vehicle.availability ? 'text-green-600' : 'text-red-600'}`}>
+                                        <span className="text-gray-800">Availability:</span> {vehicle.availability ? 'Available' : 'Unavailable'}
+                                    </p>
+                                </div>
+                                <div className="space-y-2 text-gray-700">
+                                    <p><strong>Year:</strong> {specs.year}</p>
+                                    <p><strong>Fuel Type:</strong> {specs.fuelType}</p>
+                                    <p><strong>Engine Capacity:</strong> {specs.engineCapacity}</p>
+                                    <p><strong>Transmission:</strong> {specs.transmission}</p>
+                                    <p><strong>Seating Capacity:</strong> {specs.seatingCapacity}</p>
+                                    <p><strong>Color:</strong> {specs.color}</p>
+                                    <p><strong>Features:</strong> {specs.features}</p>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
