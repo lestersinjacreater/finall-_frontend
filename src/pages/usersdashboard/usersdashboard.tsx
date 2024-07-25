ce32a42a1d67d78fbe27146d590d10d0b3129fea
lestersinjacreater <lestetsinjawatosia@gmail.com>
2024-07-25 04:10:06 +0300

    pundott

--- src/pages/usersdashboard/usersdashboard.tsx
+++ src/pages/usersdashboard/usersdashboard.tsx
@@ -1,13 +1,13 @@
 import Userdashnavigation from "./userdashnav"
 import Sidebar from "./sidebar"
-import Profile from "../userprofile/userprofile"
+//import Profile from "../userprofile/userprofile"
 
 const userdashboard = () => {
     return (
         <div>
             <Userdashnavigation />
             <Sidebar />
-            <Profile />
+            
 
 
 
